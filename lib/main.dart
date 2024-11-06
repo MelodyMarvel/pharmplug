7380de3de9e70f6a44732348f1902a53f7291cc9
Okogun Melody <okogunmelody@gmail.com>
2024-11-06 21:39:07 +0100

    home committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:pharmplug/home/welcome_screen.dart';
 import 'package:pharmplug/onboarding/lab_test_frame.dart';
 import 'package:pharmplug/onboarding/splash.dart';
 import 'package:pharmplug/onboarding/onboarding_screen2.dart';
@@ -17,10 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-     
-      home: const UserScreen(),
+      debugShowCheckedModeBanner: false,     
+      home:  HomeScreen(),
     );
   }
 }
